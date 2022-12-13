e351b2cf9f87243ef84636de41467e94c9ffef8a
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-12-13 11:14:55 +0900

    Create Santa_2022.py
    load image data

---
+++ Santa_2022.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# load image data
+df_image=pd.read_csv('image.csv')
+df_image
+# -
+
+
